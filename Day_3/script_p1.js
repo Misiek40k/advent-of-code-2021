f529e74dbfbf65279f71e0f2b35c609faa49aec8
Misiek40k <comanderrr@gmail.com>
2021-12-14 12:24:27 +0100

    Add day_3_p1

---
+++ Day_3/script_p1.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const bitCalculation = (fileName) => {
+    try {
+        const dataArr = fs.readFileSync(fileName, 'utf8').toString().split('\n');
+        const bitsArr = dataArr.map(value => value.split(''));
+        const bitsCountArr = [];
+
+        let gammaRate = '';
+        let epsilonRate = '';
+
+        bitsArr.forEach(bitNumber => {
+            bitNumber.forEach((number, index) =>{
+                if (number === '1') {
+                    bitsCountArr[index] = bitsCountArr[index] ? bitsCountArr[index] + 1 : 1;
+                }
+            });
+        })
+
+        bitsCountArr.forEach((value)=>{
+            if (value > bitsArr.length / 2) {
+                gammaRate += '1'
+                epsilonRate += '0'
+            } else {
+                gammaRate += '0'
+                epsilonRate += '1'
+            }
+        });
+
+        return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
+    } catch {
+        console.log('Error! No File');
+    }
+}
+
+console.log(bitCalculation('input.txt'));
